87e3dec1e4734801eb3cf4e4eee96f06ba9d3ed5
Kanwei Li <kanwei@gmail.com>
2014-03-28 00:14:02 -0400

    Cljs kickoff

---
+++ project.clj
@@ -0,0 +1,21 @@
+(defproject puertorico "0.1.0-SNAPSHOT"
+  :description "Puerto Rico Board Game in Clojure/Clojurescript"
+  :url "https://github.com/kanwei/puertorico"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2173"]
+                 [ring "1.2.2"]]
+  :plugins [[lein-cljsbuild "1.0.2"]
+            [lein-ring "0.8.10"]]
+  :hooks [leiningen.cljsbuild]
+  :source-paths ["src/clj"]
+  :cljsbuild { 
+    :builds {
+      :main {
+        :source-paths ["src/cljs"]
+        :compiler {:output-to "resources/public/js/cljs.js"
+                   :optimizations :simple
+                   :pretty-print true}
+        :jar true}}}
+  :main puertorico.server
+  :ring {:handler puertorico.server/app})
+
