9338d86a7d4f4a1c03d9fff807ae5513cb26c6a4
Kanwei Li <kanwei@gmail.com>
2014-09-30 21:43:07 -0400

    Deps

--- project.clj
+++ project.clj
@@ -2,13 +2,13 @@
   :description "Puerto Rico Board Game in Clojure/Clojurescript"
   :url "https://github.com/kanwei/puertorico"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2202"]
-                 [ring "1.2.2"]
-                 [compojure "1.1.6"]
+                 [org.clojure/clojurescript "0.0-2356"]
+                 [ring "1.3.1"]
+                 [compojure "1.1.9"]
                  [reagent "0.4.2"]
-                 [http-kit "2.1.18"]]
+                 [http-kit "2.1.19"]]
   :plugins [[lein-cljsbuild "1.0.3"]
-            [lein-ring "0.8.10"]]
+            [lein-ring "0.8.12"]]
   :hooks []
   :source-paths ["src/clj" "src/common"]
   
